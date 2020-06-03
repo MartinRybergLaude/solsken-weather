8d58f21013e78da4c1ffeda1263804aa9db30427
martinryberglaude <martin.ryberg.laude@gmail.com>
2020-06-03 15:08:28 +0200

    fix width of sidebar at larger screen sizes

--- src/screens/Weather/fragments/FragmentSidebar.tsx
+++ src/screens/Weather/fragments/FragmentSidebar.tsx
@@ -1,23 +1,50 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styles from './FragmentSidebar.module.scss'
 
 import { motion, AnimatePresence } from 'framer-motion'
 import Search from '../components/Search';
+import useWindowDimensions from 'utils/useDimensions';
 
 interface Props {
     visible: boolean
     setVisibility: Function
 }
-const variantsSidebar = ({
-    visible: { left: 0 },
-    hidden:  { left: "-80%"}
-});
 
 export default function Sidebar(props: Props) {
+    
+    // State to change animation variable hidden
+    const [moveLeft, setMoveLeft] = useState("-80%")
+    
+    // Animation variables for hiding and showing
+    const variantsSidebar = ({
+        visible: { left: 0 },
+        hidden:  { left: moveLeft}
+    })
 
+    // Detect window resizes to change animation variable
+    useEffect(() => {
+        setCorrectMoveLeft()
+        function handleResize() {
+            setCorrectMoveLeft()
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    function setCorrectMoveLeft() {
+        if (window.innerWidth >= 512) {
+            console.log("width > 512")
+            setMoveLeft("-400px")
+        } else {
+            console.log("width < 512")
+            setMoveLeft("-80%")
+        }
+    }
+    // Ref used to detect clicks outside of it
     const wrapperRef = useRef<HTMLDivElement>(null)
     useOutsideAlerter()
 
+    // Detect clicks outside wrapperRef
     function useOutsideAlerter() {
         useEffect(() => {
           function handleClickOutside(event: any) {

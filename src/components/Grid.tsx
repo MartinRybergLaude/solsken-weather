87dfdd699941f8bfe164d5576efd87d534578e8d
MartinRybergLaude <martinryberglaude@gmail.com>
2020-07-23 11:44:23 +0200

    add support for removing unavailable grid items

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next'
 import * as Consts from 'utils/constants'
 import * as FormattedWeatherData from 'model/TypesFormattedWeather'
 import * as WeatherData from 'model/TypesWeather'
+import FakeGridItem from './FakeGridItem'
 
 interface Props {
     dataFormatted: FormattedWeatherData.Hour
@@ -20,28 +21,44 @@ export default function Grid(props: Props) {
         gridClassName = styles.grid + " " + styles.gridAlt
         rowClassName = styles.row + " " + styles.rowAlt
     }
+
+    function shouldDisplayItem(data: number): boolean {
+        if (data && !isNaN(data)) {
+            return true
+        } else {
+            return false
+        }
+    }
     return (
         <div className={gridClassName}>
             <div className={rowClassName}>
-                {props.data.precMean && !isNaN(props.data.precMean) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiUmbrella} text={t("grid_prec")} value={props.dataFormatted.precMean} />
-                } {props.data.wind && !isNaN(props.data.wind) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiWind + " towards-" + props.dataFormatted.windDirDeg + "-deg"} text={props.dataFormatted.windDir} value={props.dataFormatted.wind} />
-                } {props.data.pressure && !isNaN(props.data.pressure) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiBarometer} text={t("grid_pressure")} value={props.dataFormatted.pressure} />
-                } {props.data.vis && !isNaN(props.data.vis) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_vis")} value={props.dataFormatted.vis} />
+                {shouldDisplayItem(props.data.precMean) 
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiUmbrella} text={t("grid_prec")} value={props.dataFormatted.precMean} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.wind)
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiWind + " towards-" + props.dataFormatted.windDirDeg + "-deg"} text={props.dataFormatted.windDir} value={props.dataFormatted.wind} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.pressure)
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiBarometer} text={t("grid_pressure")} value={props.dataFormatted.pressure} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.vis)
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_vis")} value={props.dataFormatted.vis} />
+                : <FakeGridItem/>
                 }
             </div>
             <div className={rowClassName}>
-                {props.data.humidity && !isNaN(props.data.humidity) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiRaindrop} text={t("grid_humidity")} value={props.dataFormatted.humidity} />
-                } {props.data.gusts && !isNaN(props.data.gusts) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiStrongwind} text={t("grid_gusts")} value={props.dataFormatted.gusts} />
-                } {props.data.cloud && !isNaN(props.data.cloud) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiCloudy} text={t("grid_cloud_cover")} value={props.dataFormatted.cloud} />
-                } {props.data.feelslike && !isNaN(props.data.feelslike) &&
-                <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
+                {shouldDisplayItem(props.data.humidity)
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiRaindrop} text={t("grid_humidity")} value={props.dataFormatted.humidity} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.gusts)
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiStrongwind} text={t("grid_gusts")} value={props.dataFormatted.gusts} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.cloud) 
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiCloudy} text={t("grid_cloud_cover")} value={props.dataFormatted.cloud} />
+                : <FakeGridItem/>
+                } {shouldDisplayItem(props.data.feelslike) 
+                ? <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
+                : <FakeGridItem/>
                 }
             </div>
         </div>

23a0eb3051d419df0793b26ace3fb347ee115e5a
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2021-10-23 14:11:02 +0200

    Fix #5

--- src/screens/Day/components/Hour.tsx
+++ src/screens/Day/components/Hour.tsx
@@ -29,52 +29,29 @@ export default function Hour(props: Props) {
         }
     }
 
-    if (props.animateExpand) { return (
-            <motion.div
-            className={styles.hour} 
-            onClick={() => setExpanded()}
-            animate={isExpandedVis ? "open" : "closed"}
-            variants={variants}
-            transition={{ type: "spring", stiffness: 1000, damping: 100 }}
-            initial={false}>
-                <div className={styles.preview}>
-                    <div className={styles.left}>
-                        <p className={styles.hourText}>{props.hourFormatted.hour}</p>
-                        <p>{props.hourFormatted.text}</p>
-                    </div>
-                    <div className={styles.right}>
-                        <p className={styles.temprText}>{props.hourFormatted.tempr}</p>
-                        <i className={styles.weatherIcon + " wi " + props.hourFormatted.icon}/>
-                    </div>    
+    return (
+        <motion.div
+        className={styles.hour} 
+        onClick={() => setExpanded()}
+        animate={isExpandedVis ? "open" : "closed"}
+        variants={variants}
+        transition={{ type: "spring", stiffness: 1000, damping: 100 }}
+        initial={false}>
+            <div className={styles.preview}>
+                <div className={styles.left}>
+                    <p className={styles.hourText}>{props.hourFormatted.hour}</p>
+                    <p>{props.hourFormatted.text}</p>
                 </div>
-                {isMountedVis &&
-                  <div className={styles.containerExpanded}>
-                    <Grid altStyle={true} dataFormatted={props.hourFormatted} data={props.hour}/>
-                  </div>
-                }
-            </motion.div>
-        )
-    } else {
-        return (
-            <div
-            className={styles.hour} 
-            onClick={() => (setMountedVis(!isMountedVis))}>
-                <div className={styles.preview}>
-                    <div className={styles.left}>
-                        <p className={styles.hourText}>{props.hourFormatted.hour}</p>
-                        <p>{props.hourFormatted.text}</p>
-                    </div>
-                    <div className={styles.right}>
-                        <p className={styles.temprText}>{props.hourFormatted.tempr}</p>
-                        <i className={" wi " + props.hourFormatted.icon}/>
-                    </div>    
-                </div>
-                {isMountedVis &&
-                  <div className={styles.containerExpanded}>
-                    <Grid altStyle={true} dataFormatted={props.hourFormatted} data={props.hour}/>
-                  </div>
-                }
+                <div className={styles.right}>
+                    <p className={styles.temprText}>{props.hourFormatted.tempr}</p>
+                    <i className={styles.weatherIcon + " wi " + props.hourFormatted.icon}/>
+                </div>    
             </div>
-        )
-    }
+            {isMountedVis &&
+                <div className={styles.containerExpanded}>
+                <Grid altStyle={true} dataFormatted={props.hourFormatted} data={props.hour}/>
+                </div>
+            }
+        </motion.div>
+    )
 }

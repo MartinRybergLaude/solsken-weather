331c808cda499ac05496015aa2b251ffa7380655
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2021-11-24 18:56:21 +0100

    Fix: Grid bugs and rearrangement

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -22,25 +22,27 @@ export default function Grid(props: Props) {
         gridClassName = styles.grid + " " + styles.gridAlt
         rowClassName = styles.row + " " + styles.rowAlt
     }
-
-    if (getItem("dataSrc") === "smhi" || "yr") {
+    
+    if (getItem("dataSrc") === "smhi" || getItem("dataSrc") === "yr") {
         return (
             <div className={gridClassName}>
                 <div className={rowClassName}>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiUmbrella} text={t("grid_prec")} value={props.dataFormatted.precMean} />
                     <GridItem altStyle={props.altStyle} icon={Consts.WiWind + " towards-" + props.dataFormatted.windDirDeg + "-deg"} text={props.dataFormatted.windDir} value={props.dataFormatted.wind} />
                     <GridItem altStyle={props.altStyle} icon={Consts.WiBarometer} text={t("grid_pressure")} value={props.dataFormatted.pressure} />
-                    {getItem("dataSrc") === "yr" ?
-                    <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_fog")} value={props.dataFormatted.fog} />
-                    :
-                    <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_vis")} value={props.dataFormatted.vis} />
-                    }
+                    <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
+                    
                 </div>
                 <div className={rowClassName}>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiRaindrop} text={t("grid_humidity")} value={props.dataFormatted.humidity} />
                     <GridItem altStyle={props.altStyle} icon={Consts.WiStrongwind} text={t("grid_gusts")} value={props.dataFormatted.gusts} />
                     <GridItem altStyle={props.altStyle} icon={Consts.WiCloudy} text={t("grid_cloud_cover")} value={props.dataFormatted.cloud} />
-                    <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
+                    {getItem("dataSrc") === "yr" ?
+                    <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_fog")} value={props.dataFormatted.fog} />
+                    :
+                    <GridItem altStyle={props.altStyle} icon={Consts.WiFog} text={t("grid_vis")} value={props.dataFormatted.vis} />
+                    }
+                    
                 </div>
             </div>
         )
@@ -49,13 +51,13 @@ export default function Grid(props: Props) {
             <div className={gridClassName}>
                 <div className={rowClassName}>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiWind + " towards-" + props.dataFormatted.windDirDeg + "-deg"} text={props.dataFormatted.windDir} value={props.dataFormatted.wind} />
-                    <FakeGridItem altStyle={props.altStyle} icon={props.dataFormatted.icon}/>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiBarometer} text={t("grid_pressure")} value={props.dataFormatted.pressure} />
+                    <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
                 </div>
                 <div className={rowClassName}>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiRaindrop} text={t("grid_humidity")} value={props.dataFormatted.humidity} />
+                    <FakeGridItem altStyle={props.altStyle} icon={props.dataFormatted.icon}/>
                     <GridItem altStyle={props.altStyle} icon={Consts.WiCloudy} text={t("grid_cloud_cover")} value={props.dataFormatted.cloud} />
-                    <GridItem altStyle={props.altStyle} icon={Consts.WiThermometer} text={t("grid_feels_like")} value={props.dataFormatted.feelslike} />
                 </div>
             </div>
         )

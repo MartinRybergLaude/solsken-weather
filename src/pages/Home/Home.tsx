0651783fd49f13d99eab5c785193a86762aa3fd6
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2022-07-22 20:17:00 +0200

    Remove unused imports

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import Card from "~/components/Card";
 import Container from "~/components/Container";
 import Layout from "~/containers/Layout";
 import TodayCard from "~/pages/Home/partials/TodayCard";
--- src/types/rawWeather.ts
+++ src/types/rawWeather.ts
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 import { WeatherIconEnum } from "~/enums/WeatherIcon";
 import {
   precUnits,

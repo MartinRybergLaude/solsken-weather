9a1bf3e8e799778ab16bacd273efec0ffc4d33bb
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2022-10-03 12:46:08 +0200

    Add service worker for offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,6 +11,29 @@ export default defineConfig({
     tsconfigPaths(),
     svgr({
       exportAsDefault: false,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "pwa-192x192.png", "pwa-512x512.png"],
+      manifest: {
+        name: "Solskeb",
+        short_name: "Solsken",
+        description: "A beautiful and smooth weather app",
+        theme_color: "#292b2f",
+        background_color: "#292b2f",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   define: {

c5cf82bb0e1bcf3572518f3c7bb4d4867540ce64
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2023-01-09 22:49:09 +0100

    fix: pwa autoreload with vite virtual module

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import "./i18n";
 import "css/global.css";
 import "css/leaflet.css";
 
 import App from "./App";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
